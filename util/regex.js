6e2b39098fd3fb7e431df0e87f902ea67235ff86
phanvanhai <phanvanhaixdddk49@gmail.com>
2024-06-11 21:25:37 +0700

    up

--- util/regex.js
+++ util/regex.js
@@ -10,7 +10,7 @@ const urlPatternHttp =
   /(https?:\/\/(?:www\.)?([-a-zA-Z0-9@:%._\+~#=]{1,256})\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*))/g;
 
 const urlPatternNoneHttp =
-  /([-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{2,}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*))/g;
+  /([^http][-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{2,}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*))/g;
 
 export function deleteSpace(content) {
   return content
@@ -32,48 +32,52 @@ function regex(html) {
     );
   }
 
-  if (patternEmail.exec(html)) {
-    html = html.replace(
-      patternEmail,
-      `<a href="mailto:$1" title="Tên miền web $2" target='_blank'>$1</a>`
-    );
-  }
-
-  if (patternYt.exec(html)) {
-    html = html.replace(
-      patternYt,
-      `<iframe width="560" height="315" src="https://www.youtube.com/embed/$5$6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
-    );
-  }
-
   return html;
 }
 
 export function convertRegexContent(blog) {
-  let match;
-
   let url = blog.content.match(urlPatternHttp);
   let urlNonHttp = blog.content.match(urlPatternNoneHttp);
 
-  if (url && !url[0].match(patternYt)) {
-    match = blog.content.replace(
-      urlPatternHttp,
-      `<a href="$1" target="_blank">$1</a>`
-    );
+  if (url) {
+    for (let i = 0; i < url.length; i++) {
+      if (patternYt.test(url[i])) {
+        blog.content = blog.content.replace(
+          patternYt,
+          `<iframe width="560" height="315" src="https://www.youtube.com/embed/$5$6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+        );
+      } else {
+        blog.content.replace(
+          url[i],
+          `<a href="${url[i]}" target="_blank">${url[i]}</a>`
+        );
+      }
+    }
   }
 
-  if (
-    urlNonHttp &&
-    !urlNonHttp[0].match(patternYt) &&
-    !urlNonHttp[0].match(patternEmail)
-  ) {
-    match = blog.content.replace(
-      urlPatternNoneHttp,
-      `<a href="https://${"$1".replace("/", "")}" target="_blank">$1</a>`
-    );
+  if (urlNonHttp) {
+    for (let i = 0; i < urlNonHttp.length; i++) {
+      if (patternEmail.test(urlNonHttp[i])) {
+        blog.content = blog.content.replace(
+          patternEmail,
+          `<a href="mailto:$1" title="Tên miền web $2" target='_blank'>$1</a>`
+        );
+      } else if (patternYt.test(urlNonHttp[i])) {
+        // console.log(urlNonHttp[i]);
+        blog.content.replace(
+          patternYt,
+          `<iframe width="560" height="315" src="https://www.youtube.com/embed/$5$6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+        );
+      } else {
+        blog.content = blog.content.replaceAll(
+          urlNonHttp[i],
+          `<a href="https://${urlNonHttp[i]}" target="_blank">${urlNonHttp[i]}</a>`
+        );
+      }
+    }
   }
 
-  return match;
+  return blog.content;
 }
 
 export default regex;
